b39972c79f18da14b23f719bf90e9844f7c9f57c
Young JIN <yjin@convoy-supply.com>
2024-10-08 15:04:50 -0700

    finished

--- WaterLogger/WaterLogger/obj/Debug/net8.0/WaterLogger.AssemblyInfo.cs
+++ WaterLogger/WaterLogger/obj/Debug/net8.0/WaterLogger.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WaterLogger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d34c495d6773ef1e0f5c37a22e8a561788521c35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68e8826f2cef543fe7309f57e9692b4975273db7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WaterLogger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WaterLogger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
